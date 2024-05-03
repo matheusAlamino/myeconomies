a591f9610bb5acf0a6164c6521432eb619aeeb48
Matheus Martins <matheus.alamino@outlook.com>
2024-05-03 14:27:33 -0300

    create user resource

---
+++ apps/api/prisma/migrations/20240503165405_add_optional_to_timestamp_user/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "Category" ALTER COLUMN "createdAt" DROP NOT NULL,
+ALTER COLUMN "updatedAt" DROP NOT NULL;
---
+++ apps/api/src/users/users.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Put,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User as UserModel } from '@prisma/client';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async signupUser(
+    @Body() userData: { name: string; email: string },
+  ): Promise<UserModel> {
+    return this.usersService.createUser(userData);
+  }
+
+  @Put(':id')
+  async updateUser(
+    @Param('id') id: string,
+    @Body() userData: { name: string; email: string },
+  ): Promise<UserModel> {
+    return this.usersService.updateUser({
+      where: { id: Number(id) },
+      data: userData,
+    });
+  }
+
+  @Delete(':id')
+  async deleteUser(@Param('id') id: string): Promise<UserModel> {
+    return this.usersService.deleteUser({ id: Number(id) });
+  }
+
+  @Get(':id')
+  async getUserById(@Param('id') id: string): Promise<UserModel> {
+    return this.usersService.user({ id: Number(id) });
+  }
+}
